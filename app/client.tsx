848f50160cfdd761ab8f8b96276b1ed466262463
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-08-11 02:50:53 +1200

    4.4 go back to hydrating from "root"

--- app/client.tsx
+++ app/client.tsx
@@ -4,6 +4,6 @@ import { createRouter } from './router'
 
 const router = createRouter()
 
-console.log('hydrating')
-// hydrateRoot(document.getElementById('root')!, <StartClient router={router} />)
-hydrateRoot(document, <StartClient router={router} />)
+console.log('hydrating on the client')
+hydrateRoot(document.getElementById('root')!, <StartClient router={router} />)
+// hydrateRoot(document, <StartClient router={router} />)
