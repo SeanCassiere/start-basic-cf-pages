dd69bbc4ad73d51cc369ba2067dcaa8628555d8f
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-08-11 02:44:05 +1200

    4.3 add a console log before hydration

--- app/client.tsx
+++ app/client.tsx
@@ -4,5 +4,6 @@ import { createRouter } from './router'
 
 const router = createRouter()
 
+console.log('hydrating')
 // hydrateRoot(document.getElementById('root')!, <StartClient router={router} />)
 hydrateRoot(document, <StartClient router={router} />)
