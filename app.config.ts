38af74e3988991a384e5c7612d5407a5402ef03c
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-11-18 14:37:07 +1300

    try these settings

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from '@tanstack/start/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import { cloudflare } from 'unenv'
 
 export default defineConfig({
   server: {
     preset: 'cloudflare-pages',
+    unenv: cloudflare,
+    cloudflare: {
+      wrangler: {
+        pages_build_output_dir: './dist',
+        compatibility_flags: ['nodejs_compat'],
+        compatibility_date: '2024-11-13',
+      },
+    },
   },
   vite: {
     plugins: [
