182d6d6983ee91aaf0615ce08e16457d24cd3fa0
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-11-18 14:39:48 +1300

    now try this

--- app.config.ts
+++ app.config.ts
@@ -6,13 +6,6 @@ export default defineConfig({
   server: {
     preset: 'cloudflare-pages',
     unenv: cloudflare,
-    cloudflare: {
-      wrangler: {
-        pages_build_output_dir: './dist',
-        compatibility_flags: ['nodejs_compat'],
-        compatibility_date: '2024-11-13',
-      },
-    },
   },
   vite: {
     plugins: [
