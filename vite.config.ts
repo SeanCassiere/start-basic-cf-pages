1db088578037608d847faaa0f89127048885add3
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-06-23 10:41:10 +1200

    feat: devinxi (#11)

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    tsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tanstackStart({
+      target: 'cloudflare-pages',
+      // prerender: {
+      //   enabled: true,
+      // },
+      // pages: [
+      //   { path: '/', prerender: { enabled: true, crawlLinks: false } },
+      //   { path: '/posts', prerender: { enabled: true, crawlLinks: false } },
+      // ],
+    }),
+  ],
+})
